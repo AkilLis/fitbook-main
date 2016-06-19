a9da2786ae84c8954ed65b90cdcf18d7cb8a6083
AkilLis <g.tuvshinbat@yahoo.com>
2016-06-19 14:16:29 +0900

    added

---
+++ resources/views/layouts/user.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html ng-app="fitwork">
+<head>
+    <title>Laravel</title>
+</head>
+<body class="nav-md">
+    <div class="container body">
+    	<div>
+    		@include('includes.user-header')
+    	</div>
+    	<div>
+    		@include('includes.user-left-side')
+    	</div>
+        <div class="main_container">
+            @yield('content')
+    	</div>
+    </div>
+</body>
+</html>
