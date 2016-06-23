20ca844048e384d6eaa45ef5d5301287b6d3dad7
FahulaS <Ahulaforever@gmail.com>
2016-06-23 12:28:02 +0900

    -upgraded scss -wizard version 1

--- resources/views/includes/user-left-side.blade.php
+++ resources/views/includes/user-left-side.blade.php
@@ -26,56 +26,51 @@
       </li>
     </ul>
 </aside>
-<section class="wizard-component">
+<section class="wizard-component" scrollable="true">
     <div id="left-menu">
       <div class="sub-left-menu scroll">
-        <div class="sbi-stage__stages-container" data-reactid="32">
-          <div class="sbi-stage__container" data-reactid="33">
-            <div class="sbi-stage sbi-stage_0 sbi-stage_completed sbi-stage_active" data-reactid="34">
-              <div class="sbi-stage__icon" data-reactid="35"></div>
-              <div class="sbi-stage__title" data-reactid="36">Profile</div>
-            </div>
-          </div>
-          <div class="sbi-stage__container" data-reactid="37">
-            <div class="sbi-stage__dot" data-reactid="38"></div>
-            <div class="sbi-stage sbi-stage_1 sbi-stage_completed" data-reactid="39">
-              <div class="sbi-stage__icon" data-reactid="40"></div>
-              <div class="sbi-stage__title" data-reactid="41">Property</div>
-            </div>
-            <div class="sbi-stage__dot" data-reactid="42"></div>
-          </div>
-          <div class="sbi-stage__container" data-reactid="43">
-            <div class="sbi-stage__dot" data-reactid="44"></div>
-            <div class="sbi-stage sbi-stage_2 sbi-stage_completed" data-reactid="45">
-              <div class="sbi-stage__icon" data-reactid="46"></div>
-              <div class="sbi-stage__title" data-reactid="47">Finances</div>
-            </div>
-            <div class="sbi-stage__dot" data-reactid="48"></div>
-          </div>
-          <div class="sbi-stage__container" data-reactid="49">
-            <div class="sbi-stage__dot" data-reactid="50"></div>
-            <div class="sbi-stage sbi-stage_3 sbi-stage_completed" data-reactid="51">
-              <div class="sbi-stage__icon" data-reactid="52"></div>
-              <div class="sbi-stage__title" data-reactid="53">Analysis</div>
-            </div>
-            <div class="sbi-stage__dot sbi-stage__dot_hidden" data-reactid="54"></div>
-          </div>
-          <div class="progress-bar__sbi-stage_container" data-reactid="55">
-            <div data-reactid="56">
-              <div class="progress-bar" data-reactid="57">
-                <div class="progress-bar__title" data-reactid="58">Your Progress</div>
-                <div class="progress-bar__track" data-reactid="59">
-                  <div class="progress-bar__fill" style="right:0%;" data-reactid="60"></div>
-                </div>
-                <div class="progress-bar__label" style="left:100%;" data-reactid="61">100%</div>
+        <div class="progress-bar__sbi-stage_container">
+          <div>
+            <div class="progress-bar">
+              <div class="progress-bar__track">
+                <div class="progress-bar__fill" style="right:0%;"></div>
               </div>
+              <!-- <div class="progress-bar__label" style="left:100%;">100%</div> -->
             </div>
           </div>
         </div>
-        <div>
+        <div class="sbi-stage__stages-container">
+          <div class="sbi-stage__container">
+            <div class="sbi-stage sbi-stage_0 sbi-stage_completed sbi-stage_active">
+              <div class="sbi-stage__icon"></div>
+              <div class="sbi-stage__title">Profile</div>
+            </div>
+          </div>
+          <div class="sbi-stage__container">
+            <div class="sbi-stage__dot"></div>
+            <div class="sbi-stage sbi-stage_1 sbi-stage_completed">
+              <div class="sbi-stage__icon"></div>
+              <div class="sbi-stage__title">Property</div>
+            </div>
+            <div class="sbi-stage__dot"></div>
+          </div>
+          <div class="sbi-stage__container">
+            <div class="sbi-stage__dot"></div>
+            <div class="sbi-stage sbi-stage_2 sbi-stage_completed">
+              <div class="sbi-stage__icon"></div>
+              <div class="sbi-stage__title">Finances</div>
+            </div>
+            <div class="sbi-stage__dot"></div>
+          </div>
+        </div>
+        <!-- Create club wizard 1 -->
+        <div style="display: none;">
           <form>
             <div class="row">
               <div class="medium-12 columns">
+                <label class="wizard-que">Клубын ID
+                  <input type="text" class="wiz-text" placeholder="Flexgym1">
+                </label>
                 <label class="wizard-que">Клубын нэр
                   <input type="text" class="wiz-text" placeholder="Flex gym, Gold's Gym, ...">
                 </label>
@@ -83,16 +78,100 @@
                   <textarea rows="4" class="wiz-textarea" placeholder="2000 онд байгуулагдсан..."></textarea>
                 </label>
                 <label class="wizard-que wiz-more"><a>Дэлгэрэнгүй мэдээлэл</a></label>
-                <label class="wizard-que">Клубын ID
-                  <input type="text" class="wiz-text" placeholder="Flexgym1">
-                </label>
                 <label class="wizard-que">Холбоосууд</label>
-                <label class="wizard-que wiz-f">facebook
+                <label class="wizard-que">facebook
+                  <input type="text" class="wiz-text" placeholder="Flex gym, Gold's Gym, ...">
+                </label>
+                <label class="wizard-que">google+
+                  <input type="text" class="wiz-text" placeholder="Flex gym, Gold's Gym, ...">
+                </label>
+                <label class="wizard-que">twitter
                   <input type="text" class="wiz-text" placeholder="Flex gym, Gold's Gym, ...">
                 </label>
               </div>
             </div>
           </form>
+        </div>
+        <!-- Create clun wizard 2 -->
+        <div style="display: none;">
+          <form>
+            <div class="row">
+              <div class="medium-12 columns">
+                <label class="wizard-que">Клубын байршил
+                  <input type="text" class="wiz-text" placeholder="Flex">
+                Газрын зураг дээрээс өөрийн клубын байршлыг сонгоно уу
+                </label>
+              </div>
+            </div>
+          </form>
+        </div>
+        <!-- Create clun wizard 2 -->
+        <div>
+          <ul class="tabs" data-tabs id="example-tabs">
+            <li class="tabs-title is-active  wizard-tab"><a href="#page-header" aria-selected="true">Толгой</a></li>
+            <li class="tabs-title  wizard-tab"><a href="#page-body">Гол хэсэг</a></li>
+            <li class="tabs-title  wizard-tab"><a href="#page-footer">Хөл</a></li>
+          </ul>
+          <div class="row small-up-1 medium-up-2 large-up-2 widget-container" id="page-header">
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="This is header">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+          </div>
+          <div class="row small-up-1 medium-up-2 large-up-2 widget-container" id="page-body">
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="This is body">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+          </div>
+          <div class="row small-up-1 medium-up-2 large-up-2 widget-container" id="page-footer">
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="This is footer">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+            <div class="column">
+              <img src="//placehold.it/300x300" class="thumbnail" alt="">
+            </div>
+          </div>
         </div>
       </div>
     </div>
