48175281085f33692b470b9f07f47acdc44bdaca
Fahulas <ahulaforever@gmail.com>
2016-06-26 17:09:56 +0900

    Login page design version 1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.master-layout')
 @section('content')
-	<div class="full-body">
+	<div>
 		<div class="header-container">
-			<div class="login-name login-input login-item"><input type="text"></div>
-			<div class="login-pass login-input login-item"><input type="password"></div>
-			<div><a class="button login-item">Primary Action</a></div>
-			<div><a class="dropdown button arrow-only login-item" type="button" data-toggle="example-dropdown" data-options="align:right">
+			<div class="login-name login-input login-item"><input type="text" placeholder="Username"></div>
+			<div class="login-pass login-input login-item"><input type="password" placeholder="Password"></div>
+			<div class="login-item"><a class="button btn-fb btn-1 btn-success">Primary Action</a></div>
+			<div class="login-item"><a class="dropdown button arrow-only btn-fb btn-2 btn-success" type="button" data-toggle="example-dropdown" data-options="align:down">
 			    <span class="show-for-sr">Show menu</span>
 			</a></div>
-				<ul id="example-dropdown" class="f-dropdown dropdown-pane" data-dropdown>
-					<li><a>Facebook</a></li>
-					<li><a>Twitter</a></li>
-				</ul>
-			<div class="login-item"><a>Forgot Password</a></div>
+			<ul id="example-dropdown" class="f-dropdown dropdown-pane dropdown-menu" data-dropdown>
+				<li><a>Facebook</a></li>
+				<li><a>Twitter</a></li>
+			</ul>
+			<div class="login-item"><a class="button btn-fb btn-trans">Forgot Password</a></div>
 		</div>
 	</div>
 	<!-- <div map-lazy-load="https://maps.google.com/maps/api/js">
